aa33a6aee10c08573098984c36f3d19bde283559
edelynko <delynko@gmail.com>
2016-09-06 11:21:08 -0600

    Add files via upload

---
+++ listlayers.py
@@ -0,0 +1,30 @@
+import arcpy
+import time
+import os
+import csv
+
+arcpy.env.overwriteOutput = True
+
+mxd = arcpy.mapping.MapDocument(r'.mxd')
+lyrs = arcpy.mapping.ListLayers(mxd)
+csvFile = r".csv"
+
+lyrName = []
+lyrSrce = []
+
+for lyr in lyrs:
+    if lyr.supports("DATASOURCE"):
+        name = lyr.name
+        srce = lyr.dataSource
+        lyrName.append(name)
+        lyrSrce.append(srce)
+
+with open(str(csvFile), 'wb') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerow(["Layer", "Source"])
+    wr.writerows(zip(lyrName, lyrSrce))
+myfile.close()
+
+del mxd
+time.sleep(5)
+os.system("start " + csvFile)
